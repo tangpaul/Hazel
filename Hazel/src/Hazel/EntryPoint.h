0280e5ce11a4aad510f14c273eb2c30fa420798f
tangpeng <tp18819491316@outlook.com>
2023-01-10 17:33:12 +0800

    添加Log类，编写premake

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int args, char** argv)	
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized log!");
+	int a = 5;
+	HZ_INFO("Hello! var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
